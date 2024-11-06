033673490d8190172b29a96d8805e14abbbd66b7
LuisRQM <luisalberto20304@hotmail.com>
2024-11-05 23:35:38 -0600

    First Commit

---
+++ database/migrations/2024_11_06_050045_create_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id'); 
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->unsignedBigInteger('category_id'); 
+            $table->foreign('category_id')->references('id')->on('categories'); 
+            $table->unsignedBigInteger('supplier_id'); 
+            $table->foreign('supplier_id')->references('supplier_id')->on('suppliers');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
